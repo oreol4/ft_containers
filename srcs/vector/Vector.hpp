b4a94d6a0627ad866da76dce18dfcf038a77f20b
Greenpools Aletha <galetha@oa-g1.msk.21-school.ru>
2022-05-14 15:54:52 +0300

    20/100 complete

--- srcs/vector/Vector.hpp
+++ srcs/vector/Vector.hpp
@@ -4,26 +4,124 @@
 
 
 namespace ft {
-	template<typename T>
+	template< typename T , typename Aloc = std::allocator<T> >
 
 	class vector {
 	private:
 		T *arr;
 		size_t sz;
 		size_t cap;
-		// allocator
+		Aloc myallocator;
 	public:
-		typedef typename size_t size_type;
-		typedef typename T value_type;
-		vector() {};
-		~vector() {};
+
+		// MEMBER TYPES
+		typedef T value_type;
+		typedef Aloc allocator_type;
+		typedef typename Aloc::reference reference;
+		typedef typename Aloc::const_reference const_refernce;
+		typedef typename Aloc::pointer pointer;
+		typedef typename Aloc::const_pointer const_pointer;
+		// this place for iterators
+		typedef size_t size_type;
+
+		vector(const allocator_type& alloc = allocator_type()): arr(0), sz(0), cap(0), myallocator(alloc){};
+		vector(size_t n, const value_type& value = value_type(), const Aloc& alloc = Aloc()): sz(n), cap(n), myallocator(alloc){
+//			if (n >= cap)
+//				reserve(n);
+			arr = myallocator.allocate(n);
+			for (size_t i = 0; i < n; i++) {
+				myallocator.construct(arr + i, value);
+			}
+		};
+		~vector() {
+			myallocator.deallocate(arr, cap);
+			myallocator.destroy(arr);
+		};
 		vector &operator=(const vector &rhs) {
 			this->arr = rhs.arr;
 			this->cap = rhs.cap;
 			this->sz = rhs.sz;
 			return (*this);
 		};
+		// ITERATORS
 
+
+
+
+		// MEMBER FUNCTION -> CAPACITY
+
+		void resize(size_t n, value_type val = value_type())
+		{
+			if (n < sz) {
+				for (size_t i = n; i < sz; i++) {
+					myallocator.destroy(arr + i);
+				}
+				sz = n;
+			} else {
+				if (cap < n) {
+					reserve(cap * 2);
+					for (size_t i = sz; i < cap; i++) {
+						myallocator.construct(arr + i, val);
+						++sz;
+					}
+				}
+			}
+//			(void)val;
+//			T *newarr = myallocator.allocate(n);
+//			if (n > cap)
+//				reserve(n);
+//			try {
+//				std::uninitialized_copy(arr, arr+n, newarr);
+//			}catch(...) {
+//				myallocator.deallocate(newarr, n);
+//			}
+//			myallocator.deallocate(arr, n);
+//			myallocator.destroy(arr);
+//			arr = newarr;
+//			sz = n;
+		};
+
+		void reserve(size_t n) {
+			if (n < cap)
+				return ;
+			T *newarr = myallocator.allocate(n);
+			try {
+				std::uninitialized_copy(arr, arr+sz, newarr);
+			}catch(...)
+			{
+				myallocator.deallocate(newarr, n);
+			}
+			myallocator.deallocate(arr, sz);
+			myallocator.destroy(arr);
+			arr = newarr;
+			cap = n;
+		}
+
+		size_type size() {
+			return (sz);
+		}
+
+		size_type capacity() {
+			return (cap);
+		}
+		size_type max_size() {
+			return (myallocator.max_size());
+		}
+		bool empty() {
+			return (sz == 0);
+		}
+		// MODIFIERS
+		void push_back(const value_type& value) {
+			if (sz == cap) {
+				reserve(2 * cap);
+			}
+			arr[sz] = value;
+			++sz;
+		}
+
+		reference operator[](size_type n) {
+			return (arr[n]);
+		}
 	};
 
 }
--- srcs/vector/main.cpp
+++ srcs/vector/main.cpp
@@ -5,6 +5,70 @@
 
 int main() {
 
-	ft::vector<int> v;
+	ft::vector<int> v1(5);
+	std::vector<int> r1(5);
+	std::cout << "size = " << v1.size() << " cap = " << v1.capacity() << std::endl;
+	std::cout << "size = " << r1.size() << " cap = " << r1.capacity() << std::endl;
 
+//	v1.push_back(22);
+	for (size_t i = 0;i < 10; ++i) {
+		v1.push_back(22);
+		r1.push_back(22);
+		std::cout << "(V1) size = " << v1.size() << " cap = " << v1.capacity() << std::endl;
+		std::cout << "(R1) size = " << r1.size() << " cap = " << r1.capacity() << std::endl;
+	}
+	v1.resize(4);
+	r1.resize(4);
+	std::cout << "size = " << v1.size() << " cap = " << v1.capacity() << std::endl;
+	std::cout << "size = " << r1.size() << " cap = " << r1.capacity() << std::endl;
+//	std::cout << "size = " << v1.size() << " cap = " << v1.capacity() << std::endl;
+//	std::cout << "size = " << r1.size() << " cap = " << r1.capacity() << std::endl;
+//	for (size_t i = 0; i < v1.size(); i++) {
+//		std::cout << v1[i] << std::endl;
+//	}
+//	std::cout << "size = " << v1.size() << " cap = " << v1.capacity() << std::endl;
+//	v1.push_back(1);
+//	v1.push_back(1);
+//	v1.push_back(1);
+//	v1.push_back(1);
+//	v1.push_back(1);
+//	std::cout << v1.size() << std::endl;
+//	std::vector<int> v2;
+
+//	for (int i = 0;i < 5; i++ ) {
+//		v1.push_back(1);
+//		std::cout << "A" << std::endl;
+//	}
+//	v1.push_back(1);
+//	std::cout << v1.empty() << std::endl;
+//	std::cout << v2.max_size() << std::endl;
+//	std::cout << v1.max_size() << std::endl;
+//	std::cout << "size = " << v2.size() << " cap = " << v2.capacity() << std::endl;
+//	v2.reserve(22);
+//	std::cout << "size = " << v2.size() << " cap = " << v2.capacity() << std::endl;
+//	v2.res
+//	v2.reserve(10);
+//	std::cout << v2.size() << std::endl;
+//	std::cout << v2.capacity() << std::endl;
+//	std::cout << v.size() << std::endl;
+//	v.reserve(10);
+//	std::cout << v.capacity() << std::endl;
+//	for (int i = 0;i < 5; ++i) {
+//		v2.push_back(i);
+//	}
+//	std::cout << "size = " << v2.size() << " cap = " << v2.capacity() << std::endl;
+//	v2.resize(1);
+//	std::cout << "size = " << v2.size() << " cap = " << v2.capacity() << std::endl;
+//	for (size_t i = 0;i < v2.capacity(); i++) {
+//		std::cout << v2[i] << std::endl;
+//	}
+//	for (size_t i = 0;i < v2.capacity(); i++) {
+//		std::cout << v2[i] << std::endl;
+//	}
+//	std::cout << "resize" << std::endl;
+//	v2.resize(21);
+//	for (size_t i = 0;i < v2.capacity(); i++) {
+//		std::cout << v2[i] << std::endl;
+//	}
+//	std::cout << "size = " << v2.size() << " cap = " << v2.capacity() << std::endl;
 }
