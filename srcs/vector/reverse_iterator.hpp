50de1b1cd81399590314095ce38126c335fabb2e
oreol4 <soldatovandrey4@gmail.com>
2022-06-22 17:34:44 +0300

    check operation

--- srcs/vector/reverse_iterator.hpp
+++ srcs/vector/reverse_iterator.hpp
@@ -2,10 +2,10 @@
 # define REVERSE_ITERATOR_HPP
 
 #include "utils.hpp"
+#include "Vector.hpp"
 
-
-//namespace ft {
-	template<typename Iterator>
+namespace ft {
+	template <typename Iterator>
 	class reverse_iterator {
 	public:
 		typedef Iterator iterator_type;
@@ -20,17 +20,103 @@
 
 		explicit reverse_iterator(iterator_type it) : m_ptr(it) {};
 
-		reverse_iterator(const reverse_iterator &copy) { *this = copy; };
+		reverse_iterator(iterator_type &copy)
+		{
+			Iterator tmp = copy;
+			m_ptr = tmp;
+		};
 
+		reference	operator*() const {
+			iterator_type tmp = m_ptr;
+			return *(--tmp);
+		}
 
-		reverse_iterator base() const { return (m_ptr); }
+		reverse_iterator	operator++(int) {
+			reverse_iterator tmp = *this;
+			++(*this);
+			return tmp;
+		}
 
+		reverse_iterator&	operator++() {
+			m_ptr++;
+			return (*this);
+		}
 
+		reverse_iterator	operator--(int) {
+			reverse_iterator tmp = *this;
+			--(*this);
+			return (tmp);
+		}
+
+		reverse_iterator&	operator--() {
+			m_ptr--;
+			return (*this);
+		}
+
+		reverse_iterator	operator+ (difference_type n) const {
+			return reverse_iterator(this->m_ptr - n);
+		}
+
+		reverse_iterator	operator- (difference_type n) const {
+			return reverse_iterator(this->m_ptr + n);
+		}
+
+		reverse_iterator	operator+= (difference_type n) const {
+			m_ptr -= n;
+			return reverse_iterator(m_ptr);
+		}
+
+		reverse_iterator	operator-= (difference_type n) const {
+			m_ptr += n;
+			return reverse_iterator(m_ptr);
+		}
+
+		pointer operator-> () const {
+			return &(operator*());
+		}
+
+		reference operator[] (difference_type n) const {
+			return *(m_ptr -n);
+		}
+
+		iterator_type base() const {
+			return (this->m_ptr);
+		};
+
+		template < typename Iter >
+		bool	operator== (const reverse_iterator<Iter>& rhs) {
+			return (this->base() == rhs.base());
+		}
+//
+		template < typename Iter >
+		bool	operator!= (const reverse_iterator<Iter>& rhs) {
+			return (this->base()!=rhs.base());
+		}
+//
+		template < typename Iter >
+		bool operator< (const reverse_iterator<Iter>& rhs) {
+			return (this->base() < rhs.base());
+		}
+
+		template < typename Iter >
+		bool operator> (const reverse_iterator<Iter>& rhs) {
+			return (this->base() > rhs.base());
+		}
+
+		template < typename Iter >
+		bool operator>= (const reverse_iterator<Iter>& rhs) {
+			return (this->base() >= rhs.base());
+		}
+
+		template < typename Iter >
+		bool operator<= (const reverse_iterator<Iter>& rhs) {
+			return (this->base() <= rhs.base());
+		}
 	private:
-		pointer m_ptr;
+		Iterator m_ptr;
 	};
 
-//}
+}
 
 
 
