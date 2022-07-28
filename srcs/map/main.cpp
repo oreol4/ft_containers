95069953057bfbd3f157f0c2bef903837dbb5b00
oreol4 <soldatovandrey4@gmail.com>
2022-07-28 18:20:45 +0300

    cannot convert from node to rbt

--- srcs/map/main.cpp
+++ srcs/map/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 //#include "pair.hpp"
-#include "Map.hpp"
-
+//#include "Map.hpp"
+#include "utils/node.hpp"
 
 int main() {
-	ft::map<int, int> t;
+	/*ft::map<int, int> t;
 	t.__root->data = 4;
 	std::cout << t.__root->data << std::endl;
 	std::cout << t.__root->is_red << std::endl;
-	t.__root->insertNode(t, 44);
+	int tmp = 44;*/
+//	ft::Node<int> t;
+//	t.data = 4;
+	//std::cout << t.data << std::endl;
+	ft::RBTree<int>	my_tree;
 }
