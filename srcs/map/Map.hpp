0bb1cdf543875953e4fc08ea9f50d202d3992cda
oreol4 <soldatovandrey4@gmail.com>
2022-07-28 18:19:15 +0300

    nothing

--- srcs/map/Map.hpp
+++ srcs/map/Map.hpp
@@ -2,52 +2,32 @@
 # define BTREE_HPP
 #include <iostream>
 #include <memory>
-
+#include "utils/pair.hpp"
+#include "utils/comp.hpp"
+#include "utils/node.hpp"
 #define RED 1
 #define BLACK 0
 
 namespace ft {
-
-	/*template < typename T >
-	struct Node {
-		T data;
-		struct Node *__left;
-		struct Node *__right;
-		struct Node *__parent;
-		int	    is_red;	
-	};*/
-	
-	template < typename Key, typename T, typename Compare = std::less<Key> ,
-		 typename Alloc = std::allocator< std::pair<const Key, T> > >
+	template < typename Key, typename T, typename Compare = ft::less<Key> ,
+		 typename Alloc = std::allocator< ft::pair<const Key, T> > >
 	class map {
 		public:
-		typedef Key	key_type;
-		typedef T	mapped_type;
-		typedef	Alloc	value_type;
-		typedef Compare	key_compare;
+			typedef Key	key_type;
+			typedef T	mapped_type;
+			typedef	Alloc	value_type;
+			typedef Compare	key_compare;
 		//typedef		value_compare; todo this
-		typedef Alloc					allocator_type;
-		typedef typename allocator_type::reference reference;
-		typedef typename allocator_type::const_reference const_reference;
-		typedef typename allocator_type::pointer	  pointer;
-		typedef typename allocator_type::const_pointer const_pointer;
-		//iterators .
-		// difference_type
-		// size_type
-	//	private:
-
-	//	template < typename T >
-		struct Node {
-			T data;
-			struct Node *__left;
-			struct Node *__right;
-			struct Node *__parent;
-			int	    is_red;	
-		};
-		//private:
-		size_t		__size;
-		allocator_type	__alloc;
-		Node	*__root;
+			typedef Alloc					allocator_type;
+			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::const_reference const_reference;
+			typedef typename allocator_type::pointer	  pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+			
+		private:
+			size_t		__size;
+			allocator_type	__alloc;
+			Node	*__root;
 		explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()){
 			__root = new Node;
 			__alloc = alloc;
